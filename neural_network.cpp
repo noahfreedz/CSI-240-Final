d11b2a9910dc4f9cbfdd5c575600730dd931c65c
Noah Freedley <42617616+KiritoBro2004@users.noreply.github.com>
2024-11-08 12:51:39 -0500

    refactored to suit more scenarios

--- neural_network.cpp
+++ neural_network.cpp
@@ -105,8 +105,8 @@ unordered_map<int, double> average(vector<unordered_map<int, double>>& _vector)
 
 int main() {
     // Paths to your MNIST files
-    std::string imageFilePath = "C:\\coding shit\\AI\\CSI-240-Final\\cmake-build-debug\\train-images.idx3-ubyte";
-    std::string labelFilePath = "C:\\coding shit\\AI\\CSI-240-Final\\cmake-build-debug\\train-labels.idx1-ubyte";
+    std::string imageFilePath = "C:\\Users\\nfree\\OneDrive\\Desktop\\SCOTTS-BRANCH\\cmake-build-debug\\train-images.idx3-ubyte";
+    std::string labelFilePath = "C:\\Users\\nfree\\OneDrive\\Desktop\\SCOTTS-BRANCH\\cmake-build-debug\\train-labels.idx1-ubyte";
 
     // Read images and labels
     int numImages = 100000; // Change this to read as many as you need
@@ -119,10 +119,20 @@ int main() {
     // Create and train the network
     NeuralNetwork myNN(784, 2, 16, 10); // 784 input nodes for 28x28 images
     std::vector<std::unordered_map<int, double>> allWeights;
-   int count = 0;
+    int count = 0;
     for (int i = 0; i < images.size(); ++i) {
-        myNN.run_network(images[i]);
-        std::unordered_map<int, double> newWeights = myNN.backPropigation(labels[i]); // Use the label as the correct node
+        vector<double> correct_outputs;
+        correct_outputs.reserve(10);
+        for(int output = 0; output < 10; output++) {
+            cout << "OUTPUT: " << output << " - LABEL: " << labels[i] << endl;
+            if(labels[i] == output) {
+                correct_outputs[output] = 1.0;
+            } else {
+                correct_outputs[output] = 0.0;
+            }
+        }
+        myNN.run_network(images[i], correct_outputs);
+        std::unordered_map<int, double> newWeights = myNN.backpropigate_network(); // Use the label as the correct node
         allWeights.emplace_back(newWeights);
         count++;
 
@@ -131,10 +141,10 @@ int main() {
         if(count == 100)
         {
             std::unordered_map<int, double> averagedWeights = average(allWeights);
-            myNN.assignValues(averagedWeights);
+            myNN.edit_weights(averagedWeights);
             allWeights.clear();
             averagedWeights.clear();
-            cout << "Redoing Weights!" << endl;
+            cout << "Editing Weights!" << endl;
             std::this_thread::sleep_for(std::chrono::seconds(1));
             count = 0;
         }
--- neural_network.h
+++ neural_network.h
@@ -40,7 +40,7 @@ class Node {
         int layer;
         double bias;
         double activation_value;
-        double nodeError = 0;
+        double error_value = 0;
 
         // Constructor
         Node(int node_layer) : activation_value(0.0f){
@@ -66,24 +66,24 @@ class Node {
             activation_value = x;
         }
 
-        // Calculate Activation Value for Node
-        void calculate_node() {
-            double connection_total = 0;
-            if(layer != 0)
-            {for (const auto& pair : backward_connections) {
-                    //std::cout << pair.second->start_address->ID << ": " << pair.second->weight << std::endl;
-                    connection_total += pair.second->start_address->activation_value * pair.second->weight;
-                }
-            }
-
-//
-            if(layer > 0) {
-                activation_value = sigmoid(connection_total-bias);
-            }
-            else {
-                cout << "| ERROR - NODE LAYER IS OUTSIDE NEURAL NETWORK" << endl;
+    // Calculate Activation Value for Node
+    void calculate_node() {
+        double connection_total = 0;
+        if(layer != 0)
+        {for (const auto& pair : backward_connections) {
+                //std::cout << pair.second->start_address->ID << ": " << pair.second->weight << std::endl;
+                connection_total += pair.second->start_address->activation_value * pair.second->weight;
             }
         }
+
+//
+        if(layer > 0) {
+            activation_value = sigmoid(connection_total-bias);
+        }
+        else {
+            cout << "| ERROR - NODE LAYER IS OUTSIDE NEURAL NETWORK" << endl;
+        }
+    }
 };
 
 // Initialize Static Node Variables
@@ -98,24 +98,18 @@ class NeuralNetwork {
         int trys = 0;
         int correct = 0;
 
-        NeuralNetwork(int iNode_count=1, int hLayer_count=1, int hNode_count=1, int oNode_count=1) {
+        NeuralNetwork(int iNode_count, int hLayer_count, int hNode_count, int oNode_count) {
             // Reserve For No Resizing
             allConnections.reserve(999999);
 
-            // Assign Variables
-            input_node_count = iNode_count;
-            hidden_layer_count = hLayer_count;
-            hidden_node_count = hNode_count;
-            output_node_count = oNode_count;
-
-            // Input Layers
+            // Input Layer Nodes
             for(int n=0; n<iNode_count; n++) {
                 Node newInputNode(0);
                 allNodes.push_back(newInputNode);
             }
             Node::last_layer += 1;
 
-            // Hidden Layer
+            // Hidden Layer(s) Nodes
             for (int l = 0; l < hLayer_count; l++) {
                 for (int n = 0; n < hNode_count; n++) {
                     Node newHiddenNode(Node::last_layer);
@@ -124,24 +118,20 @@ class NeuralNetwork {
                 Node::last_layer += 1;
             }
 
-            // Output Layer
+            // Output Layer Nodes
             for(int n=0; n<oNode_count; n++) {
                 Node newOutputNode(Node::last_layer);
                 allNodes.push_back(newOutputNode);
             }
 
-            cout << "THIS NN HAS " << Node::last_layer << "(" << Node::last_layer+1 << ")" << " LAYERS" << endl;
-
-            // Make Connections
+            // Connections
             int current_layer = 0;
             while (current_layer < Node::last_layer) {
-                //cout << "MAKING CONNECTIONS FOR LAYER " << current_layer << endl;
                 vector<Node*> startNodes;
                 vector<Node*> endNodes;
 
                 // Collect pointers to nodes in the current and next layer
                 for (auto& node : allNodes) {
-                    //cout << "Checking Node: " << node.ID << endl;
                     int layer = node.layer;
                     if (layer == current_layer) {
                         startNodes.push_back(&node);
@@ -150,76 +140,51 @@ class NeuralNetwork {
                     }
                 }
 
-                for (auto& start_Node : startNodes)
+                // Make Connections For All Nodes
+                for (auto& start_node : startNodes)
                 {
-                    for (auto& end_Node : endNodes) {
+                    for (auto& end_node : endNodes) {
+                        // Initialize Connection
                         connection new_connection{};
 
+                        // Unique ID per Connection
                         new_connection.ID = connection::nextID;
                         connection::nextID++;
 
-                        new_connection.start_address = start_Node;
-                        new_connection.end_address = end_Node;
+                        // Initialize Connection Values
+                        new_connection.start_address = start_node;
+                        new_connection.end_address = start_node;
                         new_connection.weight = getRandom(-1.0, 1.0);
 
-                       // cout << "ID: " << new_connection.ID << " Weight " << new_connection.weight << endl;
-
+                        // Add to All Connections Hash
                         allConnections[new_connection.ID] = new_connection;
 
-                        int start_size = start_Node->forward_connections.size();
-                        start_Node->forward_connections[start_size] = & allConnections[new_connection.ID];
-                        //connection* forward_connection = start_Node->forward_connections.at(start_size);
-                        //cout << "FORWARD (HASH): " << forward_connection->ID << " - W: " << forward_connection->weight << endl;
-
-                        int end_size = end_Node->backward_connections.size();
-                        end_Node->backward_connections[end_size] = & allConnections[new_connection.ID];
-                        //connection* backward_connection = end_Node->backward_connections.at(end_size);
-                       //cout << "BACKWARD (HASH) : " << backward_connection->ID << " - W: " << backward_connection->weight << endl;
+                        // Append to Forward Connections Of Start Node
+                        int start_size = start_node->forward_connections.size();
+                        start_node->forward_connections[start_size] = & allConnections[new_connection.ID];
+                        // Append to Backward Connections Of End Node
+                        int end_size = end_node->backward_connections.size();
+                        end_node->backward_connections[end_size] = & allConnections[new_connection.ID];
                     }
                 }
-
                 current_layer++;
             }
-
-            // Print Out All Connections
-//            for (auto& node : allNodes) {
-//                cout << "NODE ID: " << node.ID << ", LAYER: " << node.layer << ", BIAS: " << node.bias << endl;
-//
-//                // Loop for forward connections
-//                for (const auto& connection : node.forward_connections) {
-//                    cout << "  Forward Connection to Node ID: " << connection.second->end_address->ID
-//                         << " with initial weight: " << connection.second->weight << endl;
-//                }
-//
-//                // Loop for backward connections
-//                for (const auto& connection : node.backward_connections) {
-//                    cout << "  Backward Connection from Node ID: " << connection.second->start_address->ID
-//                         << " with initial weight: " << connection.second->weight << endl;
-//                }
-//            }
-
         }
 
-        void run_network(vector<double> inputs) {
+        void run_network(vector<double> inputs, vector<double> correct_outputs) {
             cout << "RUNNING NETWORK" << endl;
-
-            // Set Activations of Input Layer
-            //cout << "NODE INPUTS: " << input_node_count << endl;
 
             int inputIndex = 0;
             for (auto& node : allNodes) {
                 if (node.layer == 0) {
                     node.setActivationValue(inputs[inputIndex]);
-                    //cout << "NODE ID: " << node.ID << " SET TO " << inputs[inputIndex] << endl;
                     inputIndex++;
                 }
             }
 
             // Starting with 2nd Layer, Calculate Activations
             int current_layer = 1;
-            //cout << " -- CALCULATING ACTIVATIONS -- " << endl;
             while (current_layer <= Node::last_layer) {
-                //cout << "CALCULATING LAYER " << current_layer << endl;
                 for (auto &node: allNodes) {
                     int layer = node.layer;
                     if (layer == current_layer) {
@@ -228,149 +193,73 @@ class NeuralNetwork {
                 }
                 current_layer++;
             }
+
+            // Calculate Error for all Output Nodes
+            int output_count = 0;
+            double total_error = 0.0;
             for(auto &node : allNodes) {
                 if(node.layer == Node::last_layer) {
-                    //cout << "OUTPUT OF NODE " << node.ID << ": " << node.activation_value << endl;
+                    // Calculate Target Value
+                    double target_val = correct_outputs[output_count] - node.activation_value;
+                    // Calculate Node Error Value
+                    node.error_value = node.activation_value * (1 - node.activation_value) * (target_val);
+                    total_error += std::abs(node.error_value);
+                    output_count++;
                 }
             }
+            double average_error = (output_count > 0) ? total_error / output_count : 0.0;
+            double correctness = (1.0 - average_error) * 100;
+            cout << "NETWORK FINISHED - CORRECTNESS: " << correctness << "%" << endl;
         }
 
-        int nodes_in_layer(int layer) {
-            int count = 0;
-            for(auto &node : allNodes) {
-                if (node.layer == layer) {
-                    count++;
-                }
-            }
-            return count;
-        }
-        int getPositionInLayer(int nodeId, int layer)
-        {
-            int nodelayerCount = 0;
-            for(auto node: allNodes)
+        unordered_map<int, double> backpropigate_network()
             {
-                if(node.layer == layer)
+                // New Weights To Implement
+                unordered_map<int, double> newWeights;
+                // Learning Rate 1 for default
+                double learningRate = 1;
+                // Increment Networks Try Count
+                trys++;
+
+                // Loop Through Layers Starting with Second To Last Going Backward
+                for(int i = Node::last_layer - 1; 0 < i ; i--)
                 {
-                    nodelayerCount++;
-                    if(node.ID == nodeId)
-                    {
-                        return nodelayerCount;
-                    }
-                }
-            }
-
-            return -1;
-
-        }
-
-    unordered_map<int, double> backPropigation(int correctNode)
-        {
-            double learningRate = 1;
-            double correctValue = 1;
-            double wrongValue = 0;
-            trys ++;
-            unordered_map<int, double> newWeights;
-            int Highest = 0;
-            Node *highestNode;
-            for(auto node: allNodes)
-            {
-
-                if(node.layer == Node::last_layer)
-                {
-                  if(node.activation_value > Highest)
-                  {
-                    highestNode = &node;
-                    Highest = node.activation_value;
-                  }
-                }
-
-            }
-            int correctCheck = getPositionInLayer(highestNode->ID, Node::last_layer);
-            if(correctCheck == correctNode+1)
-            {
-                correct ++;
-            }
-
-            for(int i = Node::last_layer; 0 < i ; i--)
-            {
-                for(auto& upperNode: allNodes) {
-                    if (upperNode.layer == i) {
-
-                        double targetValue = 0.0;
-
-                        // sets the output layer Errors
-                        if(i == Node::last_layer) {
-
-                            int nodePos = getPositionInLayer(upperNode.ID, i);
-                            if (nodePos == correctNode)
-                            {
-                                targetValue = correctValue - upperNode.activation_value;
+                    for(auto& node: allNodes) {
+                        if (node.layer == i && node.layer != Node::last_layer){
+                            // Sum Error Values of Previous Layer to Get Error From Current Node
+                            double error_sum = 0;
+                            for(auto _connection : node.forward_connections) {
+                                error_sum += _connection.second->weight * _connection.second->end_address->error_value;
                             }
-                            else
-                            {
-                                targetValue = wrongValue - upperNode.activation_value;
-                            }
-
-                            upperNode.nodeError = upperNode.activation_value * (1 - upperNode.activation_value) * (targetValue);
+                            // Set Error Value
+                            node.error_value = node.activation_value*(1-node.activation_value)*(error_sum);
                         }
-                        else
-                        {
-                            double sumErrorvaules = 0;
-                            for(int hash_key = 0; hash_key < upperNode.forward_connections.size(); hash_key++) {
-                                connection* _connection = upperNode.forward_connections.at(hash_key);
-                                sumErrorvaules += _connection->weight * _connection->end_address->nodeError;
-                            }
-                            //for(auto forwardConnect : upperNode.forward_connections)
-                            //{
-                            //   sumErrorvaules += forwardConnect->weight * forwardConnect->end_address ->nodeError;
-                            //}
-                            upperNode.nodeError = upperNode.activation_value*(1-upperNode.activation_value)*(sumErrorvaules);
-                            //cout << upperNode.nodeError << endl;
-                        }
-
                     }
                 }
 
+                // Determine New Weights for All Connections
+                for (auto connection : allConnections)
+                {
+                    double nodeError = connection.second.end_address->error_value;
+                    double weightChange = learningRate * nodeError * connection.second.start_address->activation_value;
+                    double tempValue = weightChange + connection.second.weight;
+                    newWeights[connection.second.ID]  = tempValue;
+                }
+                return newWeights;
             }
 
-            for (auto connection : allConnections)
+        void edit_weights(const unordered_map<int, double>& new_weights)
             {
-                double nodeError = connection.second.end_address->nodeError;
-                double weightChange =   learningRate * nodeError * connection.second.start_address->activation_value;
-                double tempValue = weightChange + connection.second.weight;
-                newWeights[connection.second.ID]  = tempValue;
+                for (auto& connection : allConnections)
+                {
+                     connection.second.weight = new_weights.at(connection.first);
+                }
             }
-
-            return newWeights;
-        }
-
-        void assignValues(const unordered_map<int, double> averagedWeights)
-        {
-            for (auto& connection : allConnections)
-            {
-//                cout << "From connection " << connection.ID << " the weight has been changed from "
-////                     << connection.weight << " to " << averagedWeights[connection.ID]
-////                     << " that is a change of " << change << endl;
-                 connection.second.weight = averagedWeights.at(connection.first);
-            }
-    }
-private:
-        int input_node_count;
-        int hidden_layer_count;
-        int hidden_node_count; //per layer
-        int output_node_count;
 };
 
 double getRandom(double min, double max) {
-    static std::random_device rd;          // Seed source
-    static std::mt19937 gen(rd());         // Mersenne Twister generator initialized with random seed
-    std::uniform_real_distribution<> dis(min, max); // Distribution in the range [min, max]
-    return dis(gen);
+    static std::random_device rd;                            // Seed source
+    static std::mt19937 gen(rd());                      // Mersenne Twister generator initialized with random seed
+    std::uniform_real_distribution<> dis(min, max);   // Distribution in the range [min, max]
+    return dis(gen);                                     // Return Random
 }
-
-
-//NEXT
-//save start and end of each layer
-//so when iterating we can save time
-//
-//might not be worth it but who knows
